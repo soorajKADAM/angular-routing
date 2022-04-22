501ddb76606729cf129af82aa71715fba23deb16
soorajKADAM <soorajak102@gmail.com>
2022-04-22 11:01:41 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'login', component:LoginComponent},
+{path:'forgot-password', component:ForgotPasswordComponent},
+{path:'', redirectTo:'/login', pathMatch:'full'},
+{path:'**', component:NotFoundComponent},
+{path:'admin',
+canActivate:[AuthGuard],
+loadChildren:()=>import('./modules/admin/admin.module').then((m)=>m.AdminModule),}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
